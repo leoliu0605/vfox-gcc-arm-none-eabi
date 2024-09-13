885b1e3a78381ef08ee2f2a19ec09fd19bd11369
leoli <jafee201153@gmail.com>
2024-09-13 17:58:48 +0800

    chore(metadata.lua): update plugin version from 0.0.1 to 0.1.0 to reflect new changes and improvements

--- metadata.lua
+++ metadata.lua
@@ -5,7 +5,7 @@ PLUGIN = {}
 --- Plugin name
 PLUGIN.name = "gcc_arm_none_eabi"
 --- Plugin version
-PLUGIN.version = "0.0.1"
+PLUGIN.version = "0.1.0"
 --- Plugin homepage
 PLUGIN.homepage = "https://github.com/leoli0605/vfox-gcc-arm-none-eabi"
 --- Plugin license, please choose a correct license according to your needs.
