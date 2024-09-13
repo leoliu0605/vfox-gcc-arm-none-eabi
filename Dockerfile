9d0f835cb500cd52c1d03bd070d5e682f48a66d4
leoli <jafee201153@gmail.com>
2024-09-13 17:57:56 +0800

    feat: add Dockerfile and docker.sh for containerized development environment

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+FROM ubuntu:20.04
+RUN apt-get update && apt-get install -y tzdata ca-certificates
+ENV TZ=Asia/Taipei
+RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+
+ARG USER_NAME=viewsec
+ARG USER_UID=1000
+ARG USER_GID=1000
+
+RUN apt-get update && \
+    DEBIAN_FRONTEND=noninteractive apt-get install -y sudo -o Dpkg::Options::="--force-confold" && \
+    groupadd -g ${USER_GID} ${USER_NAME} && \
+    useradd -m -u ${USER_UID} -g ${USER_GID} -s /bin/bash ${USER_NAME} && \
+    echo "${USER_NAME}:123456" | chpasswd && \
+    echo "${USER_NAME} ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+
+USER ${USER_NAME}
+WORKDIR /home/${USER_NAME}
+
+RUN echo "deb [trusted=yes] https://apt.fury.io/versionfox/ /" | sudo tee /etc/apt/sources.list.d/versionfox.list && \
+    sudo apt-get update && \
+    sudo apt-get install -y vfox
+
+RUN echo "" >> ~/.bashrc && \
+    echo 'eval "$(vfox activate bash)"' >> ~/.bashrc
+
+RUN vfox add nodejs
---
+++ docker.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# ==================================================
+# If you want to use docker without sudo,
+# please run the following commands in the terminal.
+# ==================================================
+# sudo usermod -aG docker $(whoami)
+# newgrp docker
+
+IMAGE_NAME="vfox"
+USER_NAME=$(whoami)
+USER_UID=$(id -u "${USER_NAME}")
+USER_GID=$(id -g "${USER_NAME}")
+PLUGIN_NAME="gcc-arm-none-eabi" # Please change the plugin name.
+ENTRY_PATH=/home/"${USER_NAME}"/.version-fox/plugin/${PLUGIN_NAME}
+
+# docker rmi ${IMAGE_NAME} -f # If you want to rebuild the image, please uncomment this line.
+
+if [ -z "$(docker images -q ${IMAGE_NAME})" ]; then
+    docker build --no-cache \
+        --build-arg USER_NAME="${USER_NAME}" \
+        --build-arg USER_UID="${USER_UID}" \
+        --build-arg USER_GID="${USER_GID}" \
+        -t ${IMAGE_NAME} .
+fi
+
+docker run -it --rm \
+    -v "$(pwd)":${ENTRY_PATH} \
+    -w ${ENTRY_PATH} \
+    ${IMAGE_NAME} /bin/bash
